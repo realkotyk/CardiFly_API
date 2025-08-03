08efa2b6694abec181cd86548a5275da10e05d2c
Serhii Chechui <serhii.chechui@gmail.com>
2025-08-04 01:00:52 +0300

    chore(examples): improves examples files

--- examples/asyncFunction.js
+++ examples/asyncFunction.js
@@ -1,12 +1,3 @@
-// Synchronious code flow
-// console.log("Start");
-// fooSynch();
-// console.log("End");
-
-// function fooSynch() {
-//     console.log("My function operation...");
-// }
-
 // Asynchronious code flow
 console.log("Async Start");
 
---
+++ examples/syncFunction.js
@@ -0,0 +1,8 @@
+// Synchronious code flow
+console.log("Start");
+fooSynch();
+console.log("End");
+
+function fooSynch() {
+    console.log("My function operation...");
+}
