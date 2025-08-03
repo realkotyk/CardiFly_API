adc393ada8788e4badfd2913a0248c06d7e9e23c
Serhii Chechui <serhii.chechui@gmail.com>
2025-08-04 00:56:18 +0300

    refactor(env): moves the connection port also to the .env variable

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+require("dotenv").config();
 
 const dbConnection = require("./startup/db");
 
 const app = express();
-const port = 3000;
 
 app.use(morgan("dev"));
 
@@ -15,6 +15,7 @@ const usersRoute = require("./routes/users");
 // http://localhost:3000/api/users
 app.use("/api/users", usersRoute);
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
 
 module.exports = async function dbConnection() {
-    const dbConnection = await mongoose.connect(process.env.MONGODB_URL);
-
-    console.log(`Mongo connected to: ${dbConnection.connection.name}`);
+    const connection = await mongoose.connect(process.env.MONGODB_URL);
+    console.log(`Mongo connected to: ${connection.connection.name}`);
 };
