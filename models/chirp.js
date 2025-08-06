04faf361fe7ece0f5eb2c9adc7024f99631afab2
Serhii Chechui <serhii.chechui@gmail.com>
2025-08-06 04:36:12 +0300

    feat(chirps): improves the chirps model

--- models/chirp.js
+++ models/chirp.js
@@ -1,14 +1,19 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
+
 // 1. Define Schema – how will be structure document in DB
 const chirpSchema = new Schema(
     {
-        userName: String,
-        textBody: String,
-        isLiked: Boolean,
-        isDisliked: Boolean,
-        isReChirped: Boolean,
+        parentId: {
+            type: Schema.Types.ObjectId,
+            require: false,
+        },
+        author: { type: Schema.Types.ObjectId, require: true, ref: "User" },
+        textBody: { type: String, require: true, minlength: 8, maxlength: 120 },
+        likesCount: Number,
+        dislikesCount: Number,
         isPosted: Boolean,
+        pictureUrl: String,
     },
     { timestamps: true, versionKey: false }
 );
